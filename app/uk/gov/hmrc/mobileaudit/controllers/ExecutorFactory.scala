3099caff25b6cc50b92d67c3caaf1ea1c0b15749
Doug Clinton <98534+DougC@users.noreply.github.com>
2019-02-25 17:08:41 +0000

    [NGC-4894] I've pulled in the `AuditEventExecutor` from `native-apps` and tweaked it enough to get it compiling. The code will form the basis of the new service, but will need a lot of ripping apart and re-working

---
+++ app/uk/gov/hmrc/mobileaudit/controllers/ExecutorFactory.scala
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.mobileaudit.controllers
+
+import java.time.{LocalDateTime, ZoneOffset}
+
+import play.api.libs.json.Json.parse
+import play.api.libs.json._
+import play.api.{Logger, LoggerLike}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.AuditExtensions._
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.audit.model.DataEvent
+
+import scala.concurrent.{ExecutionContext, Future}
+
+case class ExecutorResponse(
+  name:         String,
+  responseData: Option[JsValue] = None,
+  cacheTime:    Option[Long] = None,
+  failure:      Option[Boolean] = None,
+  timeout:      Option[Boolean] = None)
+
+sealed trait Executor[T >: ExecutorResponse] {
+  val executionType: String
+  val executorName:  String
+  val cacheTime:     Option[Long]
+
+  def execute(cacheTime: Option[Long], data: Option[JsValue], nino: String, journeyId: Option[String])(
+    implicit hc:         HeaderCarrier,
+    ex:                  ExecutionContext): Future[Option[T]]
+}
+
+trait EventExecutor extends Executor[ExecutorResponse]
+
+case class AuditEventExecutor(auditConnector: AuditConnector, logger: LoggerLike = Logger) {
+  val executorName: String = "ngc-audit-event"
+
+  private case class ValidData(auditType: String, extraDetail: Map[String, String], tags: Map[String, String])
+
+  def execute(cacheTime: Option[Long], data: Option[JsValue], nino: String, journeyId: Option[String])(
+    implicit hc:         HeaderCarrier,
+    ex:                  ExecutionContext): Future[Option[ExecutorResponse]] = {
+    val response = data
+      .flatMap(validate)
+      .map { validData =>
+        val transactionName: String = validData.tags.getOrElse("transactionName", "explicitAuditEvent")
+        val path:            String = validData.tags.getOrElse("path", validData.auditType)
+
+        val defaultEvent = DataEvent(
+          "native-apps",
+          validData.auditType,
+          tags   = hc.toAuditTags(transactionName, path),
+          detail = hc.toAuditDetails(validData.extraDetail.toSeq: _*)
+        )
+
+        val event: DataEvent = readGeneratedAt(data, defaultEvent.eventId)
+          .map(generatedAt => defaultEvent.copy(generatedAt = new org.joda.time.DateTime(generatedAt.toInstant(ZoneOffset.UTC).toEpochMilli)))
+          .getOrElse(defaultEvent)
+
+        auditConnector.sendEvent(event)
+        ExecutorResponse(executorName, failure = Some(false))
+      }
+      .getOrElse(
+        ExecutorResponse(executorName, responseData = Some(parse("""{"error": "Bad Request"}""")), failure = Some(true))
+      )
+
+    Future.successful(Some(response))
+  }
+
+  private def validate(data: JsValue): Option[ValidData] = {
+    val maybeAuditType:            Option[String]              = (data \ "auditType").asOpt[String]
+    val maybeNewFormatExtraDetail: Option[Map[String, String]] = (data \ "detail").asOpt[Map[String, String]]
+    val maybeOldFormatExtraDetail: Option[Map[String, String]] = (data \ "nino").asOpt[String].map(nino => Map("nino" -> nino))
+    val maybeExtraDetail:          Option[Map[String, String]] = maybeNewFormatExtraDetail.orElse(maybeOldFormatExtraDetail)
+    val tags:                      Map[String, String]         = (data \ "tags").asOpt[Map[String, String]].getOrElse(Map.empty)
+
+    for {
+      auditType   <- maybeAuditType
+      extraDetail <- maybeExtraDetail
+    } yield ValidData(auditType, extraDetail, tags)
+  }
+
+  private def readGeneratedAt(data: Option[JsValue], eventId: String): Option[LocalDateTime] =
+    data
+      .flatMap(json => (json \ "generatedAt").asOpt[JsValue])
+      .flatMap { generatedAtJsValue: JsValue =>
+        generatedAtJsValue.validate[LocalDateTime] match {
+          case JsSuccess(dateTime, _) => Some(dateTime)
+          case _: JsError =>
+            logger.warn(s"""Couldn't parse generatedAt timestamp $generatedAtJsValue, defaulting to now for audit event $eventId""")
+            None
+        }
+      }
+}
