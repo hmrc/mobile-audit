cf24642bcff4662fd21328914336c415e0aa3a37
Doug Clinton <98534+DougC@users.noreply.github.com>
2019-03-04 15:41:58 +0000

    [HMA-305] Cleaned up the spec a bit

--- it/uk/gov/hmrc/mobileaudit/AuditOneISpec.scala
+++ it/uk/gov/hmrc/mobileaudit/AuditOneISpec.scala
@@ -3,57 +3,51 @@ import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.stubbing.ServeEvent
 import org.joda.time.DateTime
 import org.scalatest.OptionValues
-import play.api.Logger
 import play.api.libs.json._
 import uk.gov.hmrc.mobileaudit.controllers.IncomingEventData
-import uk.gov.hmrc.mobileaudit.stubs.AuthStub
+import uk.gov.hmrc.mobileaudit.stubs.{AuditStub, AuthStub}
 import uk.gov.hmrc.mobileaudit.utils.BaseISpec
 import uk.gov.hmrc.play.audit.model.DataEvent
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
-// TODO: This is very raw and needs a lot of re-factoring to clean it up
+/**
+  * The unit tests check that various forms of `IncomingEvent` are correctly translated the the
+  * equivalent `DataEvent`, so just do some basic sanity checks that the event that is sent matches
+  * the event we generated
+  */
 class AuditOneISpec extends BaseISpec with OptionValues {
   implicit val jodaDateReads: Reads[DateTime]  = play.api.libs.json.JodaReads.DefaultJodaDateTimeReads
   implicit val readDataEvent: Reads[DataEvent] = Json.reads
 
   "a single event sent to /audit-one" - {
-    "with just a name and audit type (the minimum possible data)" - {
-      "should " in {
-        val incomingEvent = IncomingEventData("audit-type", None, Map(), None)
+    "should be forwarded to the audit service" in {
+      val auditSource   = app.configuration.underlying.getString("auditSource")
+      val auditType     = "audit-type"
+      val testNino      = "AA100000Z"
+      val incomingEvent = IncomingEventData(auditType, None, Map(), None)
 
-        AuthStub.userIsLoggedIn("event-nino")
-        respondToAuditWithNoBody
-        respondToAuditMergedWithNoBody
+      AuthStub.userIsLoggedIn(testNino)
+      AuditStub.respondToAuditWithNoBody
+      AuditStub.respondToAuditMergedWithNoBody
 
-        val response = await(wsUrl("/mobile-audit/audit-event").post(Json.toJson(incomingEvent)))
-        response.status shouldBe 204
+      val response = await(wsUrl("/mobile-audit/audit-event").post(Json.toJson(incomingEvent)))
+      response.status shouldBe 204
 
-        val pattern = postRequestedFor(urlPathEqualTo("/write/audit"))
-          .withHeader("content-type", equalTo("application/json"))
+      verifyAuditEventWasForwarded()
 
-        wireMockServer.verify(1, pattern)
+      val auditRequest: ServeEvent = getAllServeEvents.asScala.find(_.getRequest.getUrl == "/write/audit").value
+      val dataEvent = Json.parse(auditRequest.getRequest.getBodyAsString).validate[DataEvent].get
 
-        val auditRequest: ServeEvent = asScalaBuffer(getAllServeEvents).find(_.getRequest.getUrl == "/write/audit").value
-        Logger.debug(auditRequest.getRequest.getBodyAsString)
-        val sentAuditEvent = Json.parse(auditRequest.getRequest.getBodyAsString).validate[DataEvent].get
-
-        sentAuditEvent.auditSource              shouldBe "native-apps"
-        sentAuditEvent.auditType                shouldBe incomingEvent.auditType
-        sentAuditEvent.detail.get("nino").value shouldBe "event-nino"
-      }
+      dataEvent.auditSource              shouldBe auditSource
+      dataEvent.auditType                shouldBe incomingEvent.auditType
+      dataEvent.detail.get("nino").value shouldBe testNino
     }
   }
 
-  private def respondToAuditMergedWithNoBody =
-    wireMockServer.stubFor(
-      post(urlPathEqualTo("/write/audit/merged"))
-        .willReturn(aResponse()
-          .withStatus(204)))
-
-  private def respondToAuditWithNoBody =
-    wireMockServer.stubFor(
-      post(urlPathEqualTo("/write/audit"))
-        .willReturn(aResponse()
-          .withStatus(204)))
+  private def verifyAuditEventWasForwarded(): Unit =
+    wireMockServer.verify(
+      1,
+      postRequestedFor(urlPathEqualTo("/write/audit"))
+        .withHeader("content-type", equalTo("application/json")))
 }
