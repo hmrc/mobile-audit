a49d706984ccc72d53e1ac749b9b44c9c05e8fb2
Doug Clinton <98534+DougC@users.noreply.github.com>
2019-03-05 17:51:01 +0000

    [HMA-305] Explicitly bind service locator connector

--- app/uk/gov/hmrc/mobileaudit/config/GuiceModule.scala
+++ app/uk/gov/hmrc/mobileaudit/config/GuiceModule.scala
@@ -19,6 +19,7 @@ import com.google.inject.AbstractModule
 import com.google.inject.name.Names.named
 import javax.inject.Inject
 import play.api.{Configuration, Environment}
+import uk.gov.hmrc.api.connector.{ApiServiceLocatorConnector, ServiceLocatorConnector}
 import uk.gov.hmrc.http.CorePost
 import uk.gov.hmrc.mobileaudit.controllers.api.ApiAccess
 import uk.gov.hmrc.mobileaudit.tasks.ServiceLocatorRegistrationTask
@@ -29,6 +30,7 @@ class GuiceModule @Inject()(environment: Environment, configuration: Configurati
   override def configure(): Unit = {
     bindConfigString("auditSource", "auditSource")
 
+    bind(classOf[ServiceLocatorConnector]).to(classOf[ApiServiceLocatorConnector])
     bind(classOf[CorePost]).to(classOf[WSHttpImpl])
     bind(classOf[ServiceLocatorRegistrationTask]).asEagerSingleton()
 
