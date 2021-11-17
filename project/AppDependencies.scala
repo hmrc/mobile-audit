cc61dc6db79fbad7b50feba720d7d8f98e88f642
aerodigi <54714520+aerodigi@users.noreply.github.com>
2021-11-17 14:12:01 +0000

    [HMA-4845] Play 2.8 Upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val play27Bootstrap    = "5.1.0"
-  private val playHmrcApiVersion = "6.2.0-play-27"
+  private val play28Bootstrap    = "5.16.0"
+  private val playHmrcApiVersion = "6.4.0-play-28"
   private val catsCore           = "1.6.0"
 
   private val pegdownVersion                 = "1.6.0"
@@ -19,7 +19,7 @@ object AppDependencies {
   private val refinedVersion                 = "0.9.4"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-27" % play27Bootstrap,
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % play28Bootstrap,
     "uk.gov.hmrc"   %% "play-hmrc-api"             % playHmrcApiVersion,
     "org.typelevel" %% "cats-core"                 % catsCore,
     "eu.timepit"    %% "refined"                   % refinedVersion
