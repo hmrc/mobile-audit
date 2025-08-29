bdb32822a6126a67d06e8562548faa9294d64236
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2025-08-29 15:13:30 +0100

    HMA-10577 Dependency upgrades (#40)
    * HMA-10577 Dependency upgrades
    
    * HMA-10577 Addressed PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val playBootstrapVersion = "9.13.0"
-  private val playHmrcApiVersion   = "8.2.0"
+  private val playBootstrapVersion = "10.1.0"
+  private val playHmrcApiVersion   = "8.3.0"
   private val catsCore             = "2.13.0"
 
   private val flexmarkVersion   = "0.64.8"
   private val wireMockVersion  = "2.21.0"
-  private val scalaMockVersion = "6.0.0"
+  private val scalaMockVersion = "7.4.1"
   private val refinedVersion   = "0.11.3"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % playBootstrapVersion,
     "uk.gov.hmrc"   %% "play-hmrc-api-play-30"     % playHmrcApiVersion,
     "org.typelevel" %% "cats-core"                 % catsCore,
     "eu.timepit"    %% "refined"                   % refinedVersion
